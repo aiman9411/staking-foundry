a04949cc9d3605218b984e205e28d93a327c1413
Aiman Nazmi <aimannazmi59@gmail.com>
2022-05-06 15:47:56 +0800

    import IERC20 from openzeppelin

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,33 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**
 @title Staking App
 @author Aiman Nazmi
 @notice This is a smart contract - a program that can be deployed into Ethereum blockchain
 */
-
-// @notice ERC20 Interface
-interface IERC20 {
-    function totalSupply() external view returns (uint);
-
-    function balanceOf(address account) external view returns (uint);
-
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
 
 contract Staking {
 
